487289d319aa2df745bbf255a9d8f9a55fb87b73
nicholasf <nicholas.faiz@gmail.com>
2014-09-24 17:49:59 -0700

    working on 302s

--- fake_roundtrip.go
+++ fake_roundtrip.go
@@ -1,36 +1,62 @@
-package fake_client
+package fake_round_trip
 
 import (
 	"io"
 	"net/http"
 	"net/url"
 	"strings"
+	"fmt"
 )
 
-//1. a factory for producing fake clients
-//- should take HTTP Method
-//- expected body
-//- URL (as a literal or a regex)
-//- expected response status code
-//- the expected URL parameters with corresponding assertions available via regex (can I get this to work for post vars)
-//- perhaps convenience functions for working with JSON posts?
+const REDIRECTED_LOCATION = "/new-location/"
 
-func NewFakeClient(method string, url string, document string) *http.Client {
-	fr := &FakeRoundTripper{
+func NewFakeClient(method string, url string, document string) *FakeRoundTripClient {
+	fr := &FakeRoundTrip {
 		method:     method,
 		url:        url,
 		document:   document,
 		statusCode: 200,
+		header: &http.Header{},
 	}
 
-	return &http.Client{Transport: fr}
+	fakeClient := &FakeRoundTripClient{ fakeRoundTrip: fr }
+	fakeClient.Transport = fr
+	return fakeClient
 }
 
-type FakeRoundTripper struct {
+type FakeRoundTripClient struct {
+	http.Client
+	fakeRoundTrip *FakeRoundTrip
+}
+
+func (f FakeRoundTripClient) SetStatusCode(code int) *FakeRoundTripClient {
+	f.fakeRoundTrip.statusCode = code
+
+	locationRequired := code == 302 || code == 201 || code == 202
+
+	if locationRequired {
+		setDefaultLocationHeader(&f)
+	}
+
+	return &f
+}
+
+func (f FakeRoundTripClient) SetResponseHeader(key string, value string) *FakeRoundTripClient {
+	f.fakeRoundTrip.header.Set(key, value)
+	return &f
+}
+
+func (f FakeRoundTripClient) SetURL(url string) *FakeRoundTripClient {
+	f.fakeRoundTrip.url = url
+	return &f
+}
+
+type FakeRoundTrip struct {
+	statusCode int
 	method     string
 	url        string
 	document   string
-	statusCode int
+	header 	*http.Header
 }
 
 type FakeReadCloser struct {
@@ -41,7 +67,8 @@ func (f FakeReadCloser) Close() error {
 	return nil
 }
 
-func (f FakeRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+func (f FakeRoundTrip) RoundTrip(r *http.Request) (*http.Response, error) {
+	fmt.Println("*************** ", f.url)
 	var statusCode int = f.statusCode
 
 	fr := &FakeReadCloser{
@@ -49,13 +76,25 @@ func (f FakeRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	}
 
 	expectedURL, _ := url.Parse(f.url)
-	if !urlMatches(*expectedURL, *r.URL) {
+//	fmt.Println("expectedURL: ", expectedURL)
+//	fmt.Println("actualURL: ",  *r.URL)
+	if !urlMatches(*r.URL, *expectedURL) {
 		statusCode = 404
 	}
 
-	resp := &http.Response{
+	if (statusCode == 302) {
+		f.url = r.URL.Scheme + "://" + r.URL.Host + REDIRECTED_LOCATION
+		fmt.Println("URL reset to ", f.url, " for next req")
+		fmt.Println("\n\n\n")
+	}
+
+	expectedURL, _ = url.Parse(f.url)
+	fmt.Println("expectedURL2: ", expectedURL)
+
+	resp := &http.Response {
 		Body:       fr,
 		StatusCode: statusCode,
+		Header: *f.header,
 	}
 
 	return resp, nil
@@ -63,9 +102,22 @@ func (f FakeRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 
 func urlMatches(actual url.URL, expected url.URL) bool {
 	match := (actual.Scheme == expected.Scheme) && (actual.Host == expected.Host) && (actual.Path == expected.Path) && (actual.RawQuery == expected.RawQuery)
+	fmt.Println("1: ", (actual.Scheme == expected.Scheme))
+	fmt.Println("2: ", (actual.Host == expected.Host))
+	fmt.Println("3: ", (actual.Path == expected.Path))
+	fmt.Println("actual.Path ", actual.Path)
+	fmt.Println("expected.Path ", expected.Path)
+
+	fmt.Println("4: ", (actual.RawQuery == expected.RawQuery))
+
 	if match {
 		return true
 	}
 
 	return false
+}
+
+//per handle location headers: http://en.wikipedia.org/wiki/HTTP_location
+func setDefaultLocationHeader(f *FakeRoundTripClient) {
+	f.SetResponseHeader("Location", REDIRECTED_LOCATION)
 }
--- fake_roundtrip_test.go
+++ fake_roundtrip_test.go
@@ -1,37 +1,76 @@
-package fake_client
+package fake_round_trip
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
-	"io/ioutil"
+//	"io/ioutil"
 	"testing"
+	"fmt"
 )
 
 func TestFakeRoundTrip(t *testing.T) {
 
-	Convey("Basic DSL - 3 arg creator func", t, func() {
+//	Convey("Basic DSL - 3 arg creator func", t, func() {
+//		client := NewFakeClient("GET", "https://api.opsgenie.com/v1/json/alert", "hello world")
+//		Convey("It generates a fake client", func() {
+//			So(client, ShouldNotBeNil)
+//		})
+//
+//		Convey("it returns the document with a 200", func() {
+//			resp, _ := client.Get("https://api.opsgenie.com/v1/json/alert")
+//			text, _ := ioutil.ReadAll(resp.Body)
+//			So(string(text), ShouldEqual, "hello world")
+//			So(resp.StatusCode, ShouldEqual, 200)
+//		})
+//
+//		Convey("it doesnt resolve to the wrong URL", func() {
+//			resp, _ := client.Get("https://somethingelse.com")
+//			So(resp.StatusCode, ShouldEqual, 404)
+//		})
+//	})
+
+	Convey("Chaining response setters on the client", t, func() {
 		client := NewFakeClient("GET", "https://api.opsgenie.com/v1/json/alert", "hello world")
-		Convey("It generates a fake client", func() {
-			So(client, ShouldNotBeNil)
+
+//		Convey("the header", func() {
+//			client.SetResponseHeader("Content-Type", "application/json")
+//			resp, err := client.Get("https://api.opsgenie.com/v1/json/alert")
+//			So(err, ShouldBeNil)
+//			So(resp.Header.Get("Content-Type"), ShouldEqual, "application/json")
+//		})
+//
+//		Convey("the status code, generically", func() {
+//			client = client.SetStatusCode(500)
+//			resp, err := client.Get("https://api.opsgenie.com/v1/json/alert")
+//			So(err, ShouldBeNil)
+//			So(resp.StatusCode, ShouldEqual, 500)
+//		})
+
+		Convey("the status code, with Location headers for 201, 202, 302", func() {
+//			locationRequiredCodes := []int{ 201, 302, 202 }
+			locationRequiredCodes := []int{ 302 }
+
+			for _, code := range locationRequiredCodes {
+				fmt.Println("code: ", code)
+				client = client.SetStatusCode(code)
+				resp, err := client.Get("https://api.opsgenie.com/v1/json/alert")
+				So(err, ShouldBeNil)
+				So(resp.Header.Get("Location"), ShouldNotBeNil)
+				fmt.Println("scode: ", resp.StatusCode)
+				So(resp.StatusCode, ShouldEqual, code)
+				fmt.Println("done.")
+			}
 		})
 
-		Convey("it returns the document with a 200", func() {
-			resp, _ := client.Get("https://api.opsgenie.com/v1/json/alert")
-			text, _ := ioutil.ReadAll(resp.Body)
-			So(string(text), ShouldEqual, "hello world")
-			So(resp.StatusCode, ShouldEqual, 200)
-		})
 
-		Convey("it doesnt resolve to the wrong URL", func() {
-			resp, _ := client.Get("https://somethingelse.com")
-			So(resp.StatusCode, ShouldEqual, 404)
-		})
+		//		Convey("the status code", func() {
+		//			client = client.SetStatusCode(302)
+		//		})
+
+//		Convey("it can accept a header status code", t, func() {
+//			resp, _ := client.Get("https://api.opsgenie.com/v1/json/alert")
+//			foo, _ := ioutil.ReadAll(resp.Body)
+//			So(resp, ShouldNotBeNil)
+//			So(string(foo), ShouldEqual, "hello world")
+//		})
 	})
-
-	//
-	//	Convey("it can accept a header status code", t, func() {
-	//		resp, _ := client.Get("https://api.opsgenie.com/v1/json/alert")
-	//		foo, _ := ioutil.ReadAll(resp.Body)
-	//		So(resp, ShouldNotBeNil)
-	//		So(string(foo), ShouldEqual, "hello world")
-	//	})
 }
